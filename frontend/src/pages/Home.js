4183d2f0367875e69b1b7fe1a7a8ab8f9a227f45
Brendan Ho <brendanhwz24@gmail.com>
2023-09-12 16:56:46 +0800

    <Lesson 8> Adding frontend pacakge, installing react-router-dom package for routing capabilities & functionalities, adding basic routing Navbar & root Homepage to app

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,9 @@
+const Home = () => {
+    //Returning a template upon component getting called
+    return (
+        <div className="Home">
+            <h2>Welcome to 브렌던 Homepage</h2>
+        </div>
+    )
+}
+export default Home
