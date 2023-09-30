c658c3b89a5304d6ca4b71d38d051307c0fe36fe
Brendan Ho <brendanhwz24@gmail.com>
2023-10-01 02:09:35 +0800

    Lesson 9 Added Proxy and useState & useEffect to Home component to render collection's object {title} attribute on front-end.

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,32 @@
+import { useEffect, useState } from "react"
 const Home = () => {
+    
+    //Setting up of state
+    const [workouts, setWorkouts] = useState(null)
+
+    useEffect(() => {
+        //Fetching the workouts from the backend nodeapp
+        const fetchWorkouts = async () => {
+            const response = await fetch('/api/workouts')
+            const json = await response.json()
+
+            if (response.ok) {
+                setWorkouts(json)
+            }
+        }
+        //Invoking the fetching function
+        fetchWorkouts()
+    }, [])
+
     //Returning a template upon component getting called
     return (
         <div className="Home">
-            <h2>Welcome to 브렌던 Homepage</h2>
+            <div className="workouts">
+                {workouts && workouts.map((workout) => (
+                    <p key={workout._id}>{workout.title}</p>
+                ))}
+            </div>
         </div>
     )
 }
-export default Home
+export default Home 
